ec6ff6c768987c2f53529176aca3a35819050a9e
Yatin Bawa <yatinabc3@gmail.com>
2026-01-17 00:42:41 +0530

    added github action workflow to do tests

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "@/components/ui/provider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
